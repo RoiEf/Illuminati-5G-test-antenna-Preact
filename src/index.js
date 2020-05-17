bfb48f376343e1a49bb0938a683190d9ef68f6fa
RoiEf <roi.efrati@gmail.com>
2020-05-17 23:13:58 +0300

    Basic functionality

--- src/index.js
+++ src/index.js
@@ -3,9 +3,41 @@ import { useState, useEffect } from "preact/hooks";
 
 const App = () => {
   const [textArr, setTextArr] = useState([">"]);
-  const mesagges = [];
-
-  useEffect(() => {}, [mesagges]);
+  const mesagges = [
+    "> line 1",
+    "> line 2",
+    "> line 3",
+    "> line 4",
+    "> line 5",
+    "> line 6",
+    "> line 7",
+    "> line 8",
+    "> line 9",
+    "> line 10",
+    "> line 11",
+    "> line 12",
+    "> line 13",
+    "> line 14",
+    "> line 15",
+  ];
+  let tempArr = [];
+  let index = 0;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      tempArr = [];
+      for (let a = index; a < index + 7; a++) {
+        tempArr.push(mesagges[a]);
+      }
+      tempArr.push(">_");
+      if (index < 5) {
+        index++;
+      } else {
+        index = 0;
+      }
+      setTextArr(tempArr);
+    }, 2500);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div id="app">
